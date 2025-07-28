04595c308865e78e8cc97cda74deed8faa494f02
arian <arian24b@gmail.com>
2025-07-28 16:14:38 +0330

    feat(mac): add NekoRay installer script for macOS

---
+++ mac/installer.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+# NEKORAY_URL="https://api.github.com/repos/MatsuriDayo/nekoray/releases/latest"
+NEKORAY_URL="https://api.github.com/repos/throneproj/Throne/releases/latest"
+NEKORAY_FILE_NAME="nekoray" # Throne
+NEKORAY_DESKTOPFILE="/Applications/$NEKORAY_FILE_NAME.app"
+WGET_TIMEOUT="15"
+ARCH=$(uname -m)
+TMPDIR=$(mktemp -d)
+
+# Just for fun
+# Source: https://patorjk.com/software/taag/#p=display&v=1&f=ANSI%20Shadow&t=NekoRay%20Installer
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "\n${GREEN}███╗   ██╗███████╗██╗  ██╗ ██████╗ ██████╗  █████╗ ██╗   ██╗
+████╗  ██║██╔════╝██║ ██╔╝██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝
+██╔██╗ ██║█████╗  █████╔╝ ██║   ██║██████╔╝███████║ ╚████╔╝
+██║╚██╗██║██╔══╝  ██╔═██╗ ██║   ██║██╔══██╗██╔══██║  ╚██╔╝
+██║ ╚████║███████╗██║  ██╗╚██████╔╝██║  ██║██║  ██║   ██║
+╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ███████╗██████╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██╔════╝██╔══██╗
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     █████╗  ██████╔╝
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝${NC}\n"
+
+
+# Check if installed or not
+if [ -d "$NEKORAY_DESKTOPFILE" ]; then
+  echo -e "You already have this software installed in $NEKORAY_DESKTOPFILE.\nPlease take a backup and delete it and run this script again!"
+  exit
+fi
+
+# Download NekoRay and move to current user home
+for cmd in unzip wget; do
+  if ! command -v $cmd &> /dev/null; then
+    echo -e "$cmd is not installed.\nInstall $cmd in your system.\nFor example: sudo apt install $cmd"
+    exit
+  fi
+done
+
+echo -e "Downloading the latest version of https://github.com/Mahdi-zarei/nekoray ...."
+wget --timeout=$WGET_TIMEOUT -q -O- $NEKORAY_URL \
+ | grep -E "browser_download_url.*$NEKORAY_FILE_NAME.*macos-$ARCH.zip" \
+ | cut -d '"' -f 4 \
+ | wget --timeout=$WGET_TIMEOUT -q --show-progress --progress=bar:force -O $TMPDIR/nekoray.zip -i -
+unzip $TMPDIR/nekoray.zip -d $TMPDIR && mv $TMPDIR/$NEKORAY_FILE_NAME.app /Applications/ && rm -rf $TMPDIR
+
+
+# Done
+echo -e "\nDone, type 'NekoRay' in your desktop!"
